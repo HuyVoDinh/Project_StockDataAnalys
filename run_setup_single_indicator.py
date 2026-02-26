e746025b764fa93474a27924cbbc3992d2d8684d
Wistfy <huy.vd76@gmail.com>
2026-02-26 20:41:23 +0700

    setup single indicator

---
+++ run_setup_single_indicator.py
@@ -0,0 +1,213 @@
+from time import sleep
+
+from Enum.liquidity import Liquidity, Cash_Flow, Volume
+from Enum.signal import Emplitude
+from Enum.trend import Trend, Momentum
+from Filter.ShortTerm.ShortTermTrendFilter import ShortTermTrendFilter
+from Filter.Timing.TimingFilter import TimingFilter
+from Filter.Volatility.VolatilityFilter import VolatilityFilter
+from Filter.Volume.VolumeFilter import VolumeFilter
+from Model.Company import Company, CompanyData
+from Setup.setup import *
+from StockData.stock_analyzer import StockAnalyzer
+from StockData.stock_data import StockData
+from Setup import *
+import pandas as pd
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Init save data
+    ##############  Volume  ######################
+    vol_symbol_list_1 = []
+    vol_symbol_list_2 = []
+    vol_symbol_list_3 = []
+    vol_symbol_list_4 = []
+    vol_symbol_list_5 = []
+    vol_symbol_list_6 = []
+    vol_symbol_list_7 = []
+    vol_symbol_list_8 = []
+    ##############  Volatility  ######################
+    vola_symbol_list_1 = []
+    vola_symbol_list_2 = []
+    vola_symbol_list_3 = []
+    vola_symbol_list_4 = []
+    vola_symbol_list_5 = []
+    ##############  Timing  ######################
+    timing_symbol_list_1 = []
+    timing_symbol_list_2 = []
+    timing_symbol_list_3 = []
+    timing_symbol_list_4 = []
+    timing_symbol_list_5 = []
+    ##############  ShortTerm  ######################
+    short_symbol_list_1 = []
+    short_symbol_list_2 = []
+    short_symbol_list_3 = []
+    short_symbol_list_4 = []
+    short_symbol_list_5 = []
+    short_symbol_list_6 = []
+    #####################################################################
+    stock_data = StockData()
+    stock_data.init_listing()
+    symbol_list = stock_data.listing_information_all_symbols()
+    # symbol_list = stock_data.listing_information_by_exchange("HNX")
+
+    counter = 1
+
+    for symbol in symbol_list['symbol']:
+        if counter == 19:
+            counter = 1
+            sleep(60)
+        else:
+            counter += 1
+
+        stock_analyzer = StockAnalyzer(symbol)
+        stock_analyzer.init_Stock()
+        stock_analyzer.update_data_frame("2025-12-01", "2026-02-25")
+
+        if stock_analyzer.data_frame is None:
+            continue
+
+        stock_analyzer.Calculate_Trading_Value()
+        stock_analyzer.update_full_indicator()
+        # print(stock_analyzer.data_frame)
+        comp = Company(symbol)
+        last_5 = stock_analyzer.data_frame.tail(5)
+        for index, row in last_5.iterrows():
+            compData = CompanyData()
+            compData.import_data(row)
+            compData.trading_value = row['trading_value']
+            compData.moving_average_10.ma_price = row['vol_ma10']
+            compData.moving_average_10.ma_volume = row['price_ma10']
+            compData.moving_average_10.window = 10
+            compData.moving_average_20.ma_price = row['vol_ma20']
+            compData.moving_average_20.ma_volume = row['price_ma20']
+            compData.moving_average_20.window = 20
+            compData.moving_average_50.ma_price = row['vol_ma50']
+            compData.moving_average_50.ma_volume = row['price_ma50']
+            compData.moving_average_50.window = 50
+            compData.On_Balance_Volume = row['obv']
+            compData.Volume_Oscillator = row['volume_osc']
+            compData.Accumulation_Distribution = row['ad']
+            compData.ATR_14 = row['atr_14']
+            compData.ATR_MA5 = row['atr_ma5']
+            compData.Bollinger_Bands.Middle = row['bb_middle']
+            compData.Bollinger_Bands.BB_Upper = row['bb_upper']
+            compData.Bollinger_Bands.BB_Lower = row['bb_lower']
+            compData.Donchian_Channel.Middle = row['dc_middle']
+            compData.Donchian_Channel.Upper_Channel = row['dc_upper']
+            compData.Donchian_Channel.Lower_Channel = row['dc_lower']
+            compData.RSI_14 = row['rsi_14']
+            compData.ADX_14.ADX = row['adx_14']
+            compData.ADX_14.plus_DI = row['plus_di']
+            compData.ADX_14.minus_DI = row['minus_di']
+            compData.MACD.MACD = row['macd']
+            compData.MACD.signal = row['macd_signal']
+            compData.MACD.histogram = row['macd_hist']
+            # compData.StdDev_20 =
+            comp.company_data.append(compData)
+
+        print(comp.symbol)
+
+        if volume_filter_single_indicator_1(comp) is not None:
+            vol_symbol_list_1.append(comp.symbol)
+        if volume_filter_single_indicator_2(comp) is not None:
+            vol_symbol_list_2.append(comp.symbol)
+        if volume_filter_single_indicator_3(comp) is not None:
+            vol_symbol_list_3.append(comp.symbol)
+        if volume_filter_single_indicator_4(comp) is not None:
+            vol_symbol_list_4.append(comp.symbol)
+        if volume_filter_single_indicator_5(comp) is not None:
+            vol_symbol_list_5.append(comp.symbol)
+        if volume_filter_single_indicator_6(comp) is not None:
+            vol_symbol_list_6.append(comp.symbol)
+        if volume_filter_single_indicator_7(comp) is not None:
+            vol_symbol_list_7.append(comp.symbol)
+        if volume_filter_single_indicator_8(comp) is not None:
+            vol_symbol_list_8.append(comp.symbol)
+
+        if volatility_filter_single_indicator_1(comp) is not None:
+            vola_symbol_list_1.append(comp.symbol)
+        if volatility_filter_single_indicator_2(comp) is not None:
+            vola_symbol_list_2.append(comp.symbol)
+        if volatility_filter_single_indicator_3(comp) is not None:
+            vola_symbol_list_3.append(comp.symbol)
+        if volatility_filter_single_indicator_4(comp) is not None:
+            vola_symbol_list_4.append(comp.symbol)
+        if volatility_filter_single_indicator_5(comp) is not None:
+            vola_symbol_list_5.append(comp.symbol)
+
+        if timing_filter_single_indicator_1(comp) is not None:
+            timing_symbol_list_1.append(comp.symbol)
+        if timing_filter_single_indicator_2(comp) is not None:
+            timing_symbol_list_2.append(comp.symbol)
+        if timing_filter_single_indicator_3(comp) is not None:
+            timing_symbol_list_3.append(comp.symbol)
+        if timing_filter_single_indicator_4(comp) is not None:
+            timing_symbol_list_4.append(comp.symbol)
+        if timing_filter_single_indicator_5(comp) is not None:
+            timing_symbol_list_5.append(comp.symbol)
+
+        if short_filter_single_indicator_1(comp) is not None:
+            short_symbol_list_1.append(comp.symbol)
+        if short_filter_single_indicator_2(comp) is not None:
+            short_symbol_list_2.append(comp.symbol)
+        if short_filter_single_indicator_3(comp) is not None:
+            short_symbol_list_3.append(comp.symbol)
+        if short_filter_single_indicator_4(comp) is not None:
+            short_symbol_list_4.append(comp.symbol)
+        if short_filter_single_indicator_5(comp) is not None:
+            short_symbol_list_5.append(comp.symbol)
+        if short_filter_single_indicator_6(comp) is not None:
+            short_symbol_list_6.append(comp.symbol)
+
+
+    ####### Export data
+    result_data = pd.DataFrame(vol_symbol_list_1, columns=['vol_symbol_list_1'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(vol_symbol_list_2, columns=['vol_symbol_list_2'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(vol_symbol_list_3, columns=['vol_symbol_list_3'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(vol_symbol_list_4, columns=['vol_symbol_list_4'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(vol_symbol_list_5, columns=['vol_symbol_list_5'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(vol_symbol_list_6, columns=['vol_symbol_list_6'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(vol_symbol_list_7, columns=['vol_symbol_list_7'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+
+    result_data = pd.DataFrame(vola_symbol_list_1, columns=['vola_symbol_list_1'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(vol_symbol_list_2, columns=['vol_symbol_list_2'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(vol_symbol_list_3, columns=['vol_symbol_list_3'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(vol_symbol_list_4, columns=['vol_symbol_list_4'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(vol_symbol_list_5, columns=['vol_symbol_list_5'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+
+    result_data = pd.DataFrame(timing_symbol_list_1, columns=['timing_symbol_list_1'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(timing_symbol_list_2, columns=['timing_symbol_list_2'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(timing_symbol_list_3, columns=['timing_symbol_list_3'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(timing_symbol_list_4, columns=['timing_symbol_list_4'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(timing_symbol_list_5, columns=['timing_symbol_list_5'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+
+    result_data = pd.DataFrame(short_symbol_list_1, columns=['short_symbol_list_1'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(short_symbol_list_2, columns=['short_symbol_list_2'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(short_symbol_list_3, columns=['short_symbol_list_3'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(short_symbol_list_4, columns=['short_symbol_list_4'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(short_symbol_list_5, columns=['short_symbol_list_5'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
+    result_data = pd.DataFrame(short_symbol_list_6, columns=['short_symbol_list_6'])
+    result_data.to_csv("""D:/Project/Project_StockDataAnalys/data.csv""", index=False)
